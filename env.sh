1876fa00ec5d96b2fa91c8100ed0e1d98cf71ba4
trangttt <thuytranga1@yahoo.com>
2016-11-23 09:10:49 +0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -32,22 +32,22 @@
 ###########################################################################
     alias cppcompile='c++ -std=c++11 -stdlib=libc++'
 
-    # Use sublimetext for editing config files
-    #alias zshcfg="vim ~/.zshrc"
-    #alias envcfg="vim ~/Projects/config/env.sh"
-    #alias rm='rm -i'
+    # Use vim for editing config files
+    alias zshcfg="vim ~/.zshrc"
+    alias envcfg="vim ~/Projects/config/env.sh"
+    alias rm='rm -i'
 
-    #eval `gdircolors -b ~/.dir_colors`
-    #alias ls='gls -hF --color=auto'
-    #alias grep="grep --color=always"
-    #alias egrep="egrep --color=always"
-    #alias dls="cd $HOME/Downloads"
-    #alias docs="cd $HOME/Documents"
-    #alias vimcfg="vim $HOME/dotfiles/vimrc"
-    #alias zshcfg="vim $HOME/dotfiles/zshrc"
-    #alias tmuxcfg="vim $HOME/dotfiles/tmux.conf"
-    #alias v="vim"
-    #alias g="git"
+    eval `gdircolors -b ~/.dir_colors`
+    alias ls='gls -hF --color=auto'
+    alias grep="grep --color=always"
+    alias egrep="egrep --color=always"
+    alias dls="cd $HOME/Downloads"
+    alias docs="cd $HOME/Documents"
+    alias vimcfg="vim $HOME/dotfiles/vimrc"
+    alias zshcfg="vim $HOME/dotfiles/zshrc"
+    alias tmuxcfg="vim $HOME/dotfiles/tmux.conf"
+    alias v="vim"
+    alias g="git"
 
 ###########################################################################
 #  CUSTOM INSTALLATIONS 
